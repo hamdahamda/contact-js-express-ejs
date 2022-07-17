da792e9a1f5627d573e3c954ea15db37067426cb
Hamda <88338758+hamdahamda@users.noreply.github.com>
2022-07-17 23:13:04 +0700

    expess-contact-JSON

---
+++ utils/contacts.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+// membuat folder data jika belum ada
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+// membuat file contacs.json jika belum ada
+const dataPath = "./data/contacts.json";
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, "[]", "utf-8");
+}
+
+// ambil semua data di contact.json
+const loadContact = () => {
+  const fileBuffer = fs.readFileSync("data/contacts.json", "utf-8");
+  const contacts = JSON.parse(fileBuffer);
+  return contacts;
+};
+
+// cari contact berdasarkan nama
+
+const findContact = (nama) => {
+  const contacts = loadContact();
+  const contact = contacts.find(
+    (contact) => contact.nama.toLowerCase() === nama.toLowerCase()
+  );
+  return contact;
+};
+
+// menulis / menimpa file contacts.json denga data yang baru
+const saveContacts = (contacts) => {
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+};
+
+// menambahkan data contact baru
+const addContact = (contact) => {
+  const contacts = loadContact();
+  contacts.push(contact);
+  saveContacts(contacts);
+};
+
+//cek nama  duplikat
+const cekDuplikat = (nama) => {
+  const contacts = loadContact();
+  return contacts.find((contact) => contact.nama === nama);
+};
+
+module.exports = { loadContact, findContact, addContact, cekDuplikat };
