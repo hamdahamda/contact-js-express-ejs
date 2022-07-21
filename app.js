43986c15f0d3647fc8312a773276f343d5065a65
Hamda <88338758+hamdahamda@users.noreply.github.com>
2022-07-21 07:51:59 +0700

    fix-error-heroku

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // gunakan ejs
 app.set("view engine", "ejs");
